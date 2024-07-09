afcb1b4ea9fb5816f410acbc6c36c761c9954850
gja5899 <anh-duc.thai@teliacompany.com>
2024-07-09 23:27:25 +0300

    Add globe into grid layout

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBG";
+import GridGlobe from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -11,7 +12,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -42,72 +43,71 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        `row-span-1 relative rounded-3xl group/bento hover:shadow-xl 
-        transition duration-200 shadow-input dark:shadow-none 
-        p-4 dark:bg-black dark:border-white/[0.2] bg-white border 
-        border-transparent justify-between flex flex-col 
-        space-y-4`,
+        // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
       )}
       style={{
-        background: "rgb(2,0,36)",
+        //   add these two
+        //   you can generate the color from here https://cssgradient.io/
+        background: "rgb(4,7,29)",
         backgroundColor:
-          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,54,121,1) 35%, rgba(0,212,255,1) 100%)",
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-      <div className={`${id === 6 && "flex justify-center h-full"}`}>
+      {/* add img divs */}
+      <div className={`${id === 6 && "flex justify-center"} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img
               src={img}
               alt={img}
-              className={cn(imgClassName, "object-cover, object-center")}
+              className={cn(imgClassName, "object-cover object-center ")}
             />
           )}
         </div>
         <div
           className={`absolute right-0 -bottom-5 ${
             id === 5 && "w-full opacity-80"
-          }`}
+          } `}
         >
           {spareImg && (
             <img
               src={spareImg}
               alt={spareImg}
-              className={cn("object-cover, object-center w-full h-full")}
+              //   width={220}
+              className="object-cover object-center w-full h-full"
             />
           )}
         </div>
-        <div>
-          {id === 6  && (
-            <BackgroundGradientAnimation >
-              <div 
-               className="
-                absolute z-50 flex items-center justify-center text-white font-bold
-               "
-              />
-            </BackgroundGradientAnimation>
+        {id === 6 && (
+          // add background animation , remove the p tag
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
+          </BackgroundGradientAnimation>
+        )}
+
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
           )}
-          <div className={cn(titleClassName, 
-            'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
-          )}>
-            <div className="font-sans font-extralight text-[#c1c2d3] 
-        text-sm dark:text-neutral-300 md:text-xs lg:text-base z-10">
+        >
+          {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
+          <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
             {description}
-            </div>
+          </div>
+          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
+          {/* remove mb-2 mt-2 */}
+          <div
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+          >
+            {title}
           </div>
         </div>
-      </div>
+        {id === 2 && <GridGlobe
 
-
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        <div
-          className="font-sans font-bold text-neutral-600 
-        dark:text-neutral-200 mb-2 mt-2"
-        >
-          {title}
-        </div>
-
+        />}
       </div>
     </div>
   );

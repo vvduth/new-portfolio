181466fc4734ec6517af05219bdd21daeb30fbb8
gja5899 <anh-duc.thai@teliacompany.com>
2024-08-15 12:43:50 +0300

    added footer and fix some minor bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Approach from "@/components/Approach";
 import Client from "@/components/Client";
 import Experiences from "@/components/Experiences";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProject from "@/components/RecentProject";
@@ -12,7 +13,7 @@ export default function Home() {
   return (
     <main
       className="relative bg-black-100 flex justify-center items-center flex-col
-    overflow-hidden mx-auto sm:px-10 px-5"
+      overflow-clip mx-auto sm:px-10 px-5"
     >
       <div className="max-w-7xl w-full">
         <FloatingNav
@@ -24,6 +25,7 @@ export default function Home() {
         <Client />
         <Experiences />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
